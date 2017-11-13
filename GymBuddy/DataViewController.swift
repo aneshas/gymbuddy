ad3815ba03d049fd3f53c77ce9feda4cbbab5475
Anes Hasicic <anes.hasicic@gmail.com>
2017-11-13 19:39:42 +0100

    Initial Commit

---
+++ GymBuddy/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  GymBuddy
+//
+//  Created by Anes Hasicic on 13/11/2017.
+//  Copyright © 2017 Anes Hasicic. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
